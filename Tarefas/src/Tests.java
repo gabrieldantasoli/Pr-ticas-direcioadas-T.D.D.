eaaad52bfd4e07e05e8bef699e0911a11bee839d
alexmof <alex.fidelis@ccc.ufcg.edu.br>
2024-03-03 11:26:08 -0300

    teste adição de tarefa na lista

--- Tarefas/src/Tests.java
+++ Tarefas/src/Tests.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDate;
@@ -13,7 +14,7 @@ public class Tests {
     @Before
     public void setUp() {
         tarefas = new Tarefas();
-        Tarefa tarefa1 = new Tarefa("Praticar TDD", "Implementar um gerenciador de tarefas usando TDD", LocalDate.now().plusDays(5), PRIORIDADE_ENUM.ALTA);
+        tarefa1 = new Tarefa("Praticar TDD", "Implementar um gerenciador de tarefas usando TDD", LocalDate.now().plusDays(5), PRIORIDADE_ENUM.ALTA);
     }
 
     @Test
@@ -21,4 +22,9 @@ public class Tests {
         assertTrue(tarefas.isEmpty());
     }
 
+    @Test
+    public void addTaskTest() {
+        tarefas.adicionaTarefa(tarefa1);
+        assertEquals(tarefa1, tarefas.getTarefaTitle(0));
+    }
 }
