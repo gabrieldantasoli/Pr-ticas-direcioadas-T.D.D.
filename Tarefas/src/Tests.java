30e36abe6de0f66d985a2087c2cdc6e8981f2fc4
alexmof <alex.fidelis@ccc.ufcg.edu.br>
2024-03-03 10:44:08 -0300

    testa criação de tarefa

--- Tarefas/src/Tests.java
+++ Tarefas/src/Tests.java
@@ -1,15 +1,19 @@
 import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDate;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class Tests {
 
     private Tarefas tarefas;
+    private Tarefa tarefa1;
 
     @Before
     public void setUp() {
         tarefas = new Tarefas();
+        Tarefa tarefa1 = new Tarefa("Praticar TDD", "Implementar um gerenciador de tarefas usando TDD", LocalDate.now().plusDays(5), PRIORIDADE_ENUM.ALTA);
     }
 
     @Test
