8f5f955d078c747a65079d7b44032547bca68adf
alexmof <alex.fidelis@ccc.ufcg.edu.br>
2024-03-03 19:08:09 -0300

    teste isOrdered

--- Tarefas/src/Tests.java
+++ Tarefas/src/Tests.java
@@ -3,6 +3,8 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Ignore;
@@ -13,12 +15,14 @@ public class Tests {
     private Tarefas tarefas;
     private Tarefa tarefa1;
     private Tarefa tarefa2;
+    private Tarefa tarefa3;
 
     @Before
     public void setUp() {
         tarefas = new Tarefas();
         tarefa1 = new Tarefa("Praticar TDD", "Implementar um gerenciador de tarefas usando TDD", LocalDate.now().plusDays(5), PRIORIDADE_ENUM.ALTA);
         tarefa2 = new Tarefa("Título", "Descrição", LocalDate.now().plusDays(10), PRIORIDADE_ENUM.BAIXA);
+        tarefa3 = new Tarefa("Título 3", "Descrição", LocalDate.now().plusDays(10), PRIORIDADE_ENUM.ALTA);
     }
 
     @Test
@@ -56,4 +60,15 @@ public class Tests {
         assertEquals("Título", tarefas.getTarefaTitle(0));
     }
 
+    @Test 
+    public void isOrdered() {
+        tarefas.adicionaTarefa(tarefa1);
+        tarefas.adicionaTarefa(tarefa2);
+        tarefas.adicionaTarefa(tarefa3);
+
+        ArrayList<Tarefa> tarefasList = tarefas.getTarefas();
+        assertEquals("Título 3", tarefasList.get(0).getTitle()); // Baixa
+        assertEquals("Título 2", tarefasList.get(1).getTitle()); // Média
+        assertEquals("Título 1", tarefasList.get(2).getTitle()); // Alta
+    }
 }
